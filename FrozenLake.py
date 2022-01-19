eceb4a7998e7b03eb65cd13ef79ab45034a704ec
mau-p <mauritsmerks@duck.com>
2022-01-19 17:01:59 +0100

    Fixed experimental setup

--- FrozenLake.py
+++ FrozenLake.py
@@ -1,21 +1,25 @@
+from xxlimited import new
 import gym
 
 from agents import SarsaAgent
 
-ITERATIONS = 20
+EPISODES = 100
+MAX_STEPS = 99
 
 env = gym.make('FrozenLake8x8-v1')
-env.reset()
 env.render()
 
 sarsa_agent = SarsaAgent(env, 0.95, 0.95, 0.1)
 
-for i in range(ITERATIONS):
+for episode in range(EPISODES):
     state = env.reset()
-    action = sarsa_agent.choose_egreedy(state)
-    new_state, reward, done, info = env.step(action)
-    next_action = sarsa_agent.choose_egreedy(new_state)
-    sarsa_agent.update(state, new_state, reward, action, next_action)
-    env.render()
-    if done:
-        break
+    step = 0
+    done = False
+    for step in range(MAX_STEPS):
+        action = sarsa_agent.choose_egreedy(state)
+        new_state, reward, done, info = env.step(action)
+        next_action = sarsa_agent.choose_egreedy(new_state)
+        sarsa_agent.update(state, new_state, reward, action, next_action)
+        env.render()
+        if done:
+            break
