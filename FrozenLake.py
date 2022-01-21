6e7d785c2ac8d71457b7536e2d975aba1c4528fe
mau-p <mauritsmerks@duck.com>
2022-01-21 19:54:25 +0100

    Implemented Greedy, eGreedy, UCB

--- FrozenLake.py
+++ FrozenLake.py
@@ -1,14 +1,13 @@
 import gym
 import agents
 
-EPISODES = 1000
-MAX_STEPS = 1000
+EPISODES = 10
+MAX_STEPS = 20
 
-env = gym.make('FrozenLake8x8-v1')
+env = gym.make('FrozenLake-v1')
 env.render()
 
-sarsa_agent = agents.SarsaAgent(env, 0.95, 0.95, 0.1)
-q_agent = agents.QAgent(env, 0.95, 0.95, 0.1)
+greedy_agent = agents.GreedyAgent(env, alpha=0.95, gamma=0.95)
 goal_count = 0
 
 for episode in range(EPISODES):
@@ -16,15 +15,13 @@ for episode in range(EPISODES):
     step = 0
     done = False
     for step in range(MAX_STEPS):
-        action = q_agent.choose_egreedy(state)
+        action = greedy_agent.choose(state)
         new_state, reward, done, info = env.step(action)
         if reward == 1:
             goal_count += 1
-        q_agent.update(state, action, reward, new_state)
+        greedy_agent.q_update(state, action, reward, new_state)
         env.render()
         state = new_state
         if done:
             break
-    
-print(f'goal count: {goal_count}')
-print(q_agent.Q)
+
