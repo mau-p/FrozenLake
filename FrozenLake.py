3174636bd4152238c97e338dff0db6a6a9adff52
anton <adolganov@live.nl>
2022-01-22 18:19:16 +0100

    some fixes and adjustments

--- FrozenLake.py
+++ FrozenLake.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 
 
 EPISODES = 1000
-MAX_STEPS = 20
+MAX_STEPS = 200
 
 
 env = gym.make('FrozenLake-v1', is_slippery = False)
 env.render()
-greedy_agent = agents.GreedyAgent(env, alpha=0.95, gamma=0.95, steps=MAX_STEPS, episodes=EPISODES)
+#agent = agents.GreedyAgent(env, alpha=0.95, gamma=0.95, steps=MAX_STEPS, episodes=EPISODES)
+#agent = agents.eGreedyAgent(env, epsilon=0.1, alpha=0.95, gamma=0.95,steps=MAX_STEPS,episodes=EPISODES)
+agent = agents.OptimisticAgent(env, alpha=0.95, gamma=0.95,steps=MAX_STEPS,episodes=EPISODES)
+#agent = agents.UCB(env, alpha=0.95, gamma=0.95,steps=MAX_STEPS,episodes=EPISODES)
 
 for episode in range(EPISODES):
     state = env.reset()
@@ -17,28 +20,29 @@ for episode in range(EPISODES):
     reached_goal = False
     done = False
     for step in range(MAX_STEPS):
-        action = greedy_agent.choose(state)
+        action = agent.choose(state)
         new_state, reward, done, info = env.step(action)
-        next_action = greedy_agent.choose(new_state)
+        next_action = agent.choose(new_state)
         env.render()
-        greedy_agent.sarsa_update(state, action, reward, new_state, next_action)
+        agent.sarsa_update(state, action, reward, new_state, next_action)
+        #agent.q_update(state, action, reward, new_state)
         state = new_state
         if done:
             break 
-    greedy_agent.update_stats(reward)
+    agent.update_stats(reward)
 
-plt.plot(greedy_agent.obtained_rewards, label="Amount of time it reaches the goal")
+plt.plot(agent.obtained_rewards, label="Amount of time it reaches the goal")
 plt.legend()
 plt.xlabel("Iterations")
 plt.ylabel("Number of optimal actions in %")
 plt.title(f'Number of successful runs in {EPISODES} trainings')
 plt.show()
 
-plt.plot(greedy_agent.prob_of_success, label="Amount of time it reaches the goal")
+plt.plot(agent.prob_of_success, label="xxx agent using xxx ")
 plt.legend()
 plt.xlabel("Iterations")
 plt.ylabel("Probability of the agent reaching the goal")
-plt.title("Probability of the agent reaching the goal throughout training")
+plt.title(f"Probability of the agent reaching the goal throughout {EPISODES} episodes")
 plt.show()
-
+print(agent.Q)
 
--- agents.py
+++ agents.py
@@ -5,15 +5,14 @@ class Agent:
     def __init__(self, env, alpha, gamma, steps, episodes) -> None:
         self.state_space = env.observation_space.n
         self.action_space = env.action_space.n
-        self.Q = self.init_q_values() # Set up Q table
-        self.action_count = [0] * env.action_space.n # Counts the amount of times the agent has taken each action
-        self.obtained_rewards = np.zeros(episodes) # Array that keeps track of the obtained reward per episode
-        self.prob_of_success = np.zeros(episodes) # Probability of succes at each iteration
-        self.total_success = 0 # counter of how many successes the agent has had
-        self.alpha = alpha # alpha parameter for Q and Sarsa Learning
-        self.gamma = gamma # Gamma parameter for Q and Sarsa Learning
-        self.index = 0      # Internal tracking of which episode the agent is at. 
- 
+        self.Q = self.init_q_values()  # Set up Q table
+        self.action_count = [0] * env.action_space.n  # Counts the amount of times the agent has taken each action
+        self.obtained_rewards = np.zeros(episodes)  # Array that keeps track of the obtained reward per episode
+        self.prob_of_success = np.zeros(episodes)  # Probability of succes at each iteration
+        self.total_success = 0  # counter of how many successes the agent has had
+        self.alpha = alpha  # alpha parameter for Q and Sarsa Learning
+        self.gamma = gamma  # Gamma parameter for Q and Sarsa Learning
+        self.index = 0  # Internal tracking of which episode the agent is at.
 
     def init_q_values(self):
         return np.zeros((self.state_space, self.action_space))
@@ -24,58 +23,61 @@ class Agent:
         self.Q[(state, action)] += self.alpha * (target - predict)
 
     def q_update(self, state, action, reward, new_state):
-        self.Q[(state, action)] += self.alpha * (reward + self.gamma * np.argmax(self.Q[new_state, :]) - self.Q[state, action])
+        self.Q[(state, action)] += self.alpha * (
+                    reward + self.gamma * np.argmax(self.Q[new_state, :]) - self.Q[state, action])
 
     def update_stats(self, reward):
         self.obtained_rewards[self.index] = reward
         if reward == 1:
             self.total_success += 1
         self.index += 1
-        self.prob_of_success[self.index-1] = self.total_success/self.index
+        self.prob_of_success[self.index - 1] = self.total_success / self.index
 
-        
+
 class GreedyAgent(Agent):
     def __init__(self, env, alpha, gamma, steps, episodes) -> None:
         super().__init__(env, alpha, gamma, steps, episodes)
 
     def choose(self, state):
-        return np.random.choice(np.where(self.Q[state,:] == self.Q[state,:].max())[0]) # returns random argmax
+        return np.random.choice(np.where(self.Q[state, :] == self.Q[state, :].max())[0])  # returns random argmax
+
 
 class eGreedyAgent(Agent):
-    def __init__(self, env, epsilon, alpha, gamma) -> None:
-        super().__init__(env, alpha, gamma)
+    def __init__(self, env, epsilon, alpha, gamma,steps,episodes) -> None:
+        super().__init__(env, alpha, gamma,steps,episodes)
         self.epsilon = epsilon
-    
+
     def choose(self, state):
-        if np.random.random() <= self.epsilon:
+        if np.random.random() < self.epsilon:
             return np.random.randint(self.action_space)
         else:
-            return np.random.choice(np.where(self.Q[state,:] == self.Q[state,:].max())[0]) # returns random argmax
+            return np.random.choice(np.where(self.Q[state, :] == self.Q[state, :].max())[0])  # returns random argmax
+
 
 class OptimisticAgent(Agent):
-    def __init__(self, env, alpha, gamma) -> None:
-        super().__init__(env, alpha, gamma)
+    def __init__(self, env, alpha, gamma,steps,episodes) -> None:
+        super().__init__(env, alpha, gamma,steps,episodes)
         self.Q = np.ones((self.state_space, self.action_space))
 
     def choose(self, state):
-        return np.random.choice(np.where(self.Q[state,:] == self.Q[state,:].max())[0]) # returns random argmax
+
+            return np.argmax(np.where(self.Q[state, :] == self.Q[state, :].max())[0])  # returns random argmax
 
 
 class UCB(Agent):
-    def __init__(self, env, c, alpha, gamma) -> None:
-        super().__init__(env, alpha, gamma)
-        self.c = c
+    def __init__(self, env,  alpha, gamma,steps,episodes) -> None:
+        super().__init__(env, alpha, gamma,steps,episodes)
+        self.c = np.sqrt(2*np.log(episodes)/(self.index+1))
         self.t = 1
 
     def decision(self, state):
         max_combined = -10
-        for action in enumerate(self.Q[state,:]):
+        for action in enumerate(self.Q[state, :]):
             action_counter = self.action_count[action] + 1
-            uncertainty = self.c * np.sqrt(np.log(self.t)/action_counter)
+            uncertainty = self.c * np.sqrt(np.log(self.t) / action_counter)
             if uncertainty + self.Q[(state, action)] > max_combined:
                 max_combined = uncertainty + self.Q(state, action)
                 max_combined_action = action
         self.t += 1
 
         return max_combined_action
-
